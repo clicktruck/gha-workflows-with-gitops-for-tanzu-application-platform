a81f3b4b52293e76e3e63d9efe28b8851a87e313
Chris Phillipson <cphillipson@vmware.com>
2023-01-17 12:27:31 -0800

    Fix issue in terraform/aws/cluster-addons module * The given value is not suitable for │ module.eks_blueprints_kubernetes_addons.var.crossplane_aws_provider │ declared at │ .terraform/modules/eks_blueprints_kubernetes_addons/modules/kubernetes-addons/variables.tf:228,1-35: │ attributes "enable" and "provider_aws_version" are required.

--- terraform/aws/cluster-addons/locals.tf
+++ terraform/aws/cluster-addons/locals.tf
@@ -5,6 +5,8 @@ locals {
   }
 
   crossplane_aws_provider = {
+    enable                   = true
+    provider_aws_version     = "v0.36.0"
     additional_irsa_policies = ["arn:aws:iam::aws:policy/PowerUserAccess"]
   }
 
