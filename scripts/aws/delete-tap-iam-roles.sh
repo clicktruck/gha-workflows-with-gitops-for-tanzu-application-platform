aba0e6c0bc47c537a0a95e874cc09209e3b813a4
Chris Phillipson <cphillipson@pivotal.io>
2023-01-21 17:10:38 -0800

    On destroy we pass a cluster name that is not the full cluster id
    * so we have to query for it

--- scripts/aws/delete-tap-iam-roles.sh
+++ scripts/aws/delete-tap-iam-roles.sh
@@ -7,12 +7,20 @@ set -x
 # This script is based off policy documents described in https://docs.vmware.com/en/VMware-Tanzu-Application-Platform/1.4/tap/aws-resources.html#create-iam-roles-5.
 # Use it to remove roles created by create-tap-iam-roles.sh.
 
-if [ -z "$1" ]; then
-	echo "Usage: delete-tap-iam-roles.sh {eks-cluster-name}"
+if [ -z "$1" ] && [ -z "$2" ]; then
+	echo "Usage: delete-tap-iam-roles.sh {eks-cluster-name} {aws-region}"
 	exit 1
 fi
 
-export EKS_CLUSTER_NAME="$1"
+CLUSTER_NAME_CONTAINS="$1"
+
+export AWS_REGION="$2"
+export EKS_CLUSTER_NAME=$(aws eks list-clusters --region ${AWS_REGION} --query 'clusters[?contains(@, `${CLUSTER_NAME_CONTAINS}`)]' | sed -n '2p' | tr -d '"' | awk '{gsub(/^ +| +$/,"")} {print $0}')
+
+if [ -z "$EKS_CLUSTER_NAME" ]; then
+  echo "No cluster found matching id containing $CLUSTER_NAME_CONTAINS"
+  exit 1
+fi
 
 # Check to see if tap-build-service role for cluster already exists
 aws iam get-role --role-name tap-build-service-for-$EKS_CLUSTER_NAME
