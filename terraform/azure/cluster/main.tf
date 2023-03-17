60a0bcb445deb123de463366d3379cd95b46e87f
Chris Phillipson <cphillipson@pivotal.io>
2023-03-17 06:35:33 -0700

    terraform fmt

--- terraform/azure/cluster/main.tf
+++ terraform/azure/cluster/main.tf
@@ -91,13 +91,13 @@ module "aks" {
       },
     ]
   }
-  net_profile_dns_service_ip        = "10.0.0.10"
-  net_profile_docker_bridge_cidr    = "170.10.0.1/16"
-  net_profile_service_cidr          = "10.0.0.0/16"
-  network_plugin                    = "azure"
-  network_policy                    = "azure"
-  os_disk_size_gb                   = var.aks_node_disk_size
-  sku_tier                          = "Paid"
+  net_profile_dns_service_ip     = "10.0.0.10"
+  net_profile_docker_bridge_cidr = "170.10.0.1/16"
+  net_profile_service_cidr       = "10.0.0.0/16"
+  network_plugin                 = "azure"
+  network_policy                 = "azure"
+  os_disk_size_gb                = var.aks_node_disk_size
+  sku_tier                       = "Paid"
 
   storage_profile_enabled                     = true
   storage_profile_blob_driver_enabled         = true
