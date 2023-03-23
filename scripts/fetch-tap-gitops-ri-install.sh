13fb4df924451215bca6d0ab978f90b4263f4a2c
Chris Phillipson <cphillipson@pivotal.io>
2023-03-23 13:48:02 -0700

    Add fetch-tap-gitops-ri-install.sh

---
+++ scripts/fetch-tap-gitops-ri-install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -eo pipefail
+
+# Downloads the Tanzu Gitops Reference Implementation install script and assets from Tanzu Network
+
+if [ -z "$1" ]; then
+	echo "Usage: fetch-tap-vscode-extension.sh {tanzu-network-api-token}"
+	exit 1
+fi
+
+if ! command -v pivnet &> /dev/null
+then
+    echo "Downloading pivnet CLI..."
+	curl -LO https://github.com/pivotal-cf/pivnet-cli/releases/download/v3.0.1/pivnet-linux-amd64-3.0.1
+	chmod +x pivnet-linux-amd64-3.0.1
+	sudo mv pivnet-linux-amd64-3.0.1 /usr/local/bin/pivnet
+fi
+
+
+TANZU_NETWORK_API_TOKEN="$1"
+pivnet login --api-token=$TANZU_NETWORK_API_TOKEN
+
+cd /tmp
+TAP_VERSION="1.5.0-rc.8"
+
+# Download Tanzu GitOps Reference Implementation
+TAP_PRODUCT_FILE_ID=1431307
+pivnet download-product-files --product-slug='tanzu-application-platform' --release-version="${TAP_VERSION}" --product-file-id="${TAP_PRODUCT_FILE_ID}"
+ls -la tanzu-gitops-ri-0.0.2.tgz
+
