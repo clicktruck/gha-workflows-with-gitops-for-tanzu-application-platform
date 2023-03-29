8d762f10ee007f92c7b9acbab5235144473ddaa8
Chris Phillipson <cphillipson@pivotal.io>
2023-03-29 06:34:24 -0700

    Upgrade Crossplane AWS provider to 0.38.0

--- terraform/aws/cluster-addons/locals.tf
+++ terraform/aws/cluster-addons/locals.tf
@@ -6,7 +6,7 @@ locals {
 
   crossplane_aws_provider = {
     enable                   = true
-    provider_aws_version     = "v0.36.1"
+    provider_aws_version     = "v0.38.0"
     additional_irsa_policies = ["arn:aws:iam::aws:policy/PowerUserAccess"]
   }
 
