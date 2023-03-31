b2afddf0552139a9e1584014a2e223c2a93585d7
Chris Phillipson <cphillipson@pivotal.io>
2023-03-31 05:58:52 -0700

    Upgrade TAP install to 1.5.0-rc.14

--- scripts/fetch-tap-gitops-ri-install.sh
+++ scripts/fetch-tap-gitops-ri-install.sh
@@ -21,10 +21,9 @@ TANZU_NETWORK_API_TOKEN="$1"
 pivnet login --api-token=$TANZU_NETWORK_API_TOKEN
 
 cd /tmp
-TAP_VERSION="1.5.0-rc.10"
+TAP_VERSION="1.5.0-rc.14"
 
 # Download Tanzu GitOps Reference Implementation
-TAP_PRODUCT_FILE_ID=1431307
+TAP_PRODUCT_FILE_ID=1459284
 pivnet download-product-files --product-slug='tanzu-application-platform' --release-version="${TAP_VERSION}" --product-file-id="${TAP_PRODUCT_FILE_ID}"
-ls -la tanzu-gitops-ri-0.0.2.tgz
-
+ls -la tanzu-gitops-ri-0.0.3.tgz
