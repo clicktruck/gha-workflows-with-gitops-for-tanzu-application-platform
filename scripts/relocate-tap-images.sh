2c021172e0c1c7cc66b8bb61f7cf2118aa0144ef
Chris Phillipson <cphillipson@pivotal.io>
2023-07-13 14:42:26 -0700

    Upgrade TAP install to 1.6.1-rc.4

--- scripts/relocate-tap-images.sh
+++ scripts/relocate-tap-images.sh
@@ -17,7 +17,7 @@ TARGET_REPOSITORY="${6}"
 
 # Set up environment variables for installation use by running:
 
-export TAP_VERSION=1.6.1-rc.2
+export TAP_VERSION=1.6.1-rc.4
 export TBS_FULL_DEPS_VERSION=1.10.10
 export IMGPKG_REGISTRY_HOSTNAME_0=registry.tanzu.vmware.com
 export IMGPKG_REGISTRY_USERNAME_0="${TANZUNET_USERNAME}"
